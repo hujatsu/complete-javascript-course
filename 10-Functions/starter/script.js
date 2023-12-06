9679dae890d6904d8c4ac43de32e8cf163e2e239
hujatsu <hujatsu@gmail.com>
2023-12-06 16:59:28 +0200

    section 10 code

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,322 @@
 'use strict';
+
+// 128. Default Parameters
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5 way for default values
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
+
+//   const booking = { flightNum, numPassengers, price };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
+// createBooking('LH123', undefined, 5); // skipping default parameter
+
+// 129. How passing arguments works: value vs. reference
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 234345342,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 234345342) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong passport!');
+//   }
+// };
+
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000000000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+// 131. Functions Accepting Callback Functions
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// // Higher-order function
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
+
+// // JS uses callbacks all the time
+// const high5 = function () {
+//   console.log('🖐');
+// };
+
+// document.body.addEventListener('click', high5);
+// ['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+// 132. Functions returning functions
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greeterHey = greet('Hey');
+// greeterHey('Jonas');
+// greeterHey('Steven');
+
+// greet('Hello')('Jonas');
+
+// const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
+
+// greetArrow('Hello')('Niko');
+
+// 133. The call and apply Methods
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
+
+// lufthansa.book(239, 'Jonas Schmedtmann');
+// lufthansa.book(635, 'John Smith');
+// console.log(lufthansa);
+
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
+
+// const book = lufthansa.book;
+
+// // Does NOT work
+// // book(23, 'Sarah Williams');
+
+// // call method defines that 'this' keyword refers to, here eurowings object
+// book.call(eurowings, 23, 'Sarah Williams');
+// console.log(eurowings);
+
+// book.call(lufthansa, 239, 'Mary Cooper');
+// console.log(lufthansa);
+
+// const swiss = {
+//   airline: 'Swiss Air Lines',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
+
+// book.call(swiss, 583, 'Mary Cooper');
+// console.log(swiss);
+
+// // apply method (kind of old way of doing)
+// const flightData = [583, 'George Cooper'];
+// book.apply(swiss, flightData);
+// console.log(swiss);
+
+// // more modern way than apply:
+// book.call(swiss, ...flightData);
+
+// // 134. The bind method
+// // book.call(eurowings, 23, 'Sarah Williams');
+
+// const bookEW = book.bind(eurowings);
+// const bookLH = book.bind(lufthansa);
+// const bookLX = book.bind(swiss);
+// bookEW(23, 'Steven Williams');
+// bookLX(23, 'Steven Seagulls');
+
+// // Preset arguments, here the flightNum is predefined
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Jonas Schmedtmann');
+// bookEW23('Martha Cooper');
+
+// // With Event Listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+
+//   this.planes++;
+//   console.log(this.planes);
+// };
+
+// // Does not work if we don't use the bind. The this keyword refers to the button element if we don't bind it to lufthansa
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// // Partial application
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+
+// console.log(addVAT(100));
+// console.log(addVAT(23));
+
+// // Challenge
+// const challTax = rate => value => console.log(value + value * rate);
+
+// const challAddVAT = challTax(0.23);
+// challAddVAT(100);
+
+// 135. Coding Challenge #1
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+//   // This generates [0, 0, 0, 0]. More in the next section!
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     const input = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+//       )
+//     );
+
+//     if (!isNaN(input) && this.options[input]) {
+//       this.answers[input] += 1;
+//     } else {
+//       console.log('Please provide a valid option number!');
+//     }
+
+//     this.displayResults.call(poll);
+//   },
+//   displayResults(type = 'string') {
+//     if (!['string', 'array'].includes(type)) {
+//       console.log("Allowed arguments are either 'string' or 'array'");
+//       return;
+//     }
+
+//     if (type === 'array') {
+//       console.log(this.answers);
+//       return;
+//     }
+
+//     console.log(`Poll results are ${this.answers.join(', ')}`);
+//   },
+// };
+
+// const data1 = [5, 2, 3];
+// const data2 = [1, 5, 3, 9, 6, 1];
+// document.body
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// poll.displayResults.call({ answers: data1 }, 'array');
+// poll.displayResults.call({ answers: data2 });
+
+// 136. Immediately invoked function expressions
+// const runOnce = function () {
+//   console.log('This will never run again');
+// };
+
+// runOnce();
+
+// // IIFE
+// (function () {
+//   console.log('This will never run again');
+//   const isPrivate = 23;
+// })();
+
+// (() => console.log('This will ALSO never run again'))();
+
+// 137. Closures
+// const passengerCount = 10; // demonstration, not needed
+// const secureBooking = function () {
+//   let passengerCount = 0;
+
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+
+// // see how the passengerCount in the global score is ignored and the passengerCount from the closure is used!
+// booker(); // 1
+// booker(); // 2
+// booker(); // 3
+
+// console.dir(booker);
+
+// NOTE: A function has access to the variable environment (VE) of the execution context (EC) in which it was created. Here, booker function was created in the secureBooking EC and thus has access to the passengerCount (0 at the time). The EC nor the VE do not exist at the time of booker's execution, however. This is called a closure. Closure is a VE attached to a function exactly as it was at the time and place the function was CREATED.
+
+// 138. More Closure Examples
+// let f;
+
+// // example 1
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// h();
+// f();
+
+// // Example 2
+// const boardPassengers = function (n, wait) {
+//   const perGroup = n / 3;
+
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
+//   console.log(`Will start boarding in ${wait} seconds`);
+// };
+
+// const perGroup = 1000; // will not be used, the perGroup in the closure is prioritized
+// boardPassengers(180, 3);
+
+// 139. Coding Challenge #2
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.body.addEventListener('click', () => {
+    header.style.color = 'blue';
+  });
+})();
